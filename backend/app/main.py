b4581198927e835b8dd192fba75a2deb90a97310
Galim <sadretdinovg@yandex.ru>
2026-06-08 13:09:22 +0500

    feat: базовый FastAPI + авторизация по OTP

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.database import engine, Base
+from app.models.user import User
+from app.api import auth
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="PulseHR API")
 
@@ -10,6 +15,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
--- create_str.py
+++ create_str.py
@@ -10,8 +10,24 @@ folders = [
     "frontend",
 ]
 
+init_files = [
+    "backend/app/__init__.py",
+    "backend/app/api/__init__.py",
+    "backend/app/models/__init__.py",
+    "backend/app/schemas/__init__.py",
+    "backend/app/core/__init__.py",
+]
+
+# создаём папки
 for folder in folders:
     Path(folder).mkdir(parents=True, exist_ok=True)
-    print(f"Created or exists: {folder}")
+    print(f"Folder ready: {folder}")
+
+# создаём __init__.py
+for file in init_files:
+    path = Path(file)
+    path.parent.mkdir(parents=True, exist_ok=True)  # на всякий случай
+    path.touch(exist_ok=True)  # создаёт пустой файл, если его нет
+    print(f"File ready: {file}")
 
 print("Done.")
